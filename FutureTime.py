c8aa9e62e699a236b7c42b5188430927c27d942a
NathanHeavican <nheavican@unomaha.edu>
2026-02-01 04:04:36 +0000

    Completed Lab 2

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,8 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name: Nathan Heavican
+#Date: 1/31/26
+#Assignment: Lab 2
+#Purpose: Create a program that asks the user for a number of hours and minutes, calculates what the time will be after that amount of time passes, and displays the result in HH:MM format
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -9,21 +10,22 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 6) % 12
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
+  moreHour = int(input("Enter a number of hours: "))
   #Ask user for minutes
-
+  moreMinute = int(input("Enter a number of minutes: "))
   #Calculate the time after the user-supplied time has passed.
-
+  futureMinute = (currentMinute + moreMinute) % 60
+  extraHour = (currentMinute + moreMinute) // 60
+  futureHour = (currentHour + moreHour + extraHour) % 12
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
-
-
+  print(f"The future time is {futureHour:02d}:{futureMinute:02d}")
 if __name__ == '__main__':
   main()
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,7 +1,8 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Nathan Heavican
+#Date: 1/31/26
+#Assignment: Lab 2
+#Purpose: Create a program that asks the user to enter a question, selects a random response, and displays the response
 
 #We will need random for this program, import to use this package.
 import random
@@ -9,10 +10,11 @@ import random
 def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
+  answers = ["yes", "no", "maybe", "possibly", "definitly not", "ask again later", "certainly"]
   #Prompt the user for their question.
-
+  question = input("Enter a yes or no question: ")
   #Answer question randomly with one of the options from your earlier list.
-
-
+  response = random.choice(answers)
+  print(response)
 if __name__ == '__main__':
   main()
